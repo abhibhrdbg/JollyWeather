e9590689ca47f43ca3d9025acf6b8bad5926a788
Abhishek Bhardwaj <abhishek@abhibhr.com>
2017-12-22 00:54:08 +0530

    S01.02

--- app/src/main/java/com/abhibhr/jollyweather/MainActivity.java
+++ app/src/main/java/com/abhibhr/jollyweather/MainActivity.java
@@ -2,12 +2,28 @@ package com.abhibhr.jollyweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Create a field to store the weather display TextView
+    TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Use findViewById to get a reference to the weather display TextView
+        mWeatherTextView = findViewById(R.id.tv_weather_data);
+
+        // Create an array of Strings that contain fake weather data
+        String weatherDatas[] = WeatherData.getWeatherData();
+
+        // Append each String from the fake weather data array to the TextView
+        for (String weatherData: weatherDatas){
+            mWeatherTextView.append(weatherData+"\n\n\n");
+        }
+
     }
 }
